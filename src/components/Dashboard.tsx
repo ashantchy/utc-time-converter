3747be68b9fed5e28f69bfab2adcfbc578ad1005
AshantC <ashantchaudhary22@gmail.com>
2024-12-16 20:20:07 +0545

    removed extra line of code

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,7 +4,6 @@ const Utc = () => {
   return (
     <div className="relative max-w-4xl mx-auto text-center max-h-full ">
         <h1 className="text-5xl text-blue-500 pt-52 font-light">UTC Time Zone Converter</h1>
-        <h2>Ashant</h2>
         <Time />
     </div>
   )
