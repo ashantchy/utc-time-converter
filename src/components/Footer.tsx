f978cb526fcc6d6f1a3273dcb826988453e427a2
Ashant Chaudhary <157305253+ashantchy@users.noreply.github.com>
2025-10-27 21:15:07 +0545

    Update GitHub link in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='absolute bottom-3 mx-auto text-center text-blue-500 left-1/2 -translate-x-1/2'>
-        <a href='https://github.com/ashantc' target='_blank'>https://github.com/ashantc</a>
+        <a href='https://github.com/ashantchy' target='_blank'>https://github.com/ashantchy</a>
     </div>
   )
 }
